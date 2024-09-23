91d5c5e9d95aaeeec8ba1114ebf483e48bea1791
bhmimare <bhmimare@gmail.com>
2024-09-23 08:55:32 +0200

    connect php file  with phpmyadmin

---
+++ connectphp.php
@@ -0,0 +1,25 @@
+<?php
+$dbhost='localhost';
+$dbuser='root';
+$dbpass='';
+$dbname='test';
+/* es muy importante el orden*/
+$mysqli= new mysqli ($dbhost, $dbuser,$dbpass, $dbname);
+
+  if ($mysqli->connect_errno){
+    printf("Connection failed", $mysqli ->connect_error);
+  }
+    printf( "Connect successful <br/>");
+  /* para creacion */ 
+$sql ="create table test-connection (id int , name varchar(50), fullname varchar(50))";
+  if($mysqli ->query($sql)){
+    printf("Create table perfectly ");
+  }
+  if($mysqli ->errno){
+     printf("table not created ", $mysqli ->error);
+  }
+    $mysqli->close();
+?>
+
+
+ 
